ea65212cd1e6637eab4a816845a19df1b2f4f62d
Jacob Bleser <jacobbleser@aol.com>
2016-10-03 00:29:19 +0000

    Added mongo setup files

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
+var mongoClient = require('mongodb').MongoClient;
 var express = require('express');
+var url = "mongodb://megaboy101:thejacob1@ds013916.mlab.com:13916/url-shortener";
 var app = express();
 
-app.get('/', function(req, res){
-   res.end("ONLINE!");
+mongoClient.connect(url, function(err, db){
+    
+    var shortened = db.collection('shortened');
+    
+    app.get('/', function(req, res){
+       shortened.insert({key: "Success!"})
+       
+       shortened.find({key: "Success!"}).toArray(function(err, results){
+           if (results){
+               res.end(results);
+           }
+           else {
+               res.end("error");
+            }
+       });
+    });
+   
+    db.close();
 });
+
+
 
 app.listen(process.env.PORT || 8080);
